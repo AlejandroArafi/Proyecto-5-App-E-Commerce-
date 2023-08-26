9c828dd3221f800f124f4f91e1949a92037c5273
alejandro <alejandro.arafi@gmail.com>
2023-08-26 16:26:03 -0400

    contador card

--- src/componentes/cards/Cards.jsx
+++ src/componentes/cards/Cards.jsx
@@ -1,55 +1,55 @@
-// import './Cards.css'
-// // import CAPITULO from '../../assets/images/CAPITULO.png'
-// import { Card } from '../card/Card'
-
-// export const Cards = () => {
-//   return (
-//     <>
-//     <div className="container">
-//         <Card/>
-//         <Card/>
-//         <Card/>
-//         <Card/>
-//      </div>
-
-//      <div className="container">
-//         <Card/>
-//         <Card/>
-//         <Card/>
-//         <Card/>
-//      </div>
-
-//      <div className="container">
-//         <Card/>
-//         <Card/>
-//         <Card/>
-//         <Card/>
-//      </div>
-    
-//     </>
-//   )
-// }
-
-
-import './Cards.css';
-import { Card } from '../card/Card';
-
- 
+import './Cards.css'
+// import CAPITULO from '../../assets/images/CAPITULO.png'
+import { Card } from '../card/Card'
 
 export const Cards = () => {
+  return (
+    <>
+    <div className="container">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+     </div>
+
+     <div className="container">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+     </div>
+
+     <div className="container">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+     </div>
+    
+    </>
+  )
+}
+
+
+// import './Cards.css';
+// import { Card } from '../card/Card';
+
+ 
+
+// export const Cards = () => {
   
-  const cardsPerRow = 12;
+//   const cardsPerRow = 12;
  
   
-  const cardElements = [...Array(cardsPerRow)].map((_, index) => (
-<Card key={index} />
-  ));
+//   const cardElements = [...Array(cardsPerRow)].map((_, index) => (
+// <Card key={index} />
+//   ));
 
-   return (
-<div className="container">
-<div className="card-grid">
-        {cardElements}
-</div>
-</div>
-  );
-};
+//    return (
+// <div className="container">
+// <div className="card-grid">
+//         {cardElements}
+// </div>
+// </div>
+//   );
+// };
